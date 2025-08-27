59e641d363189526f387f1e2e2e4c95e78816be7
dineshpatte <152471120+dineshpatte@users.noreply.github.com>
2025-08-27 10:15:20 +0530

    started favourites

---
+++ src/app/favourites/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="flex flex-col justify-center items-center min-h-screen bg-black text-yellow-400 font-bold text-4xl">
+        Favourites
+      </h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/Favourites.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Favourites = () => {
+  return (
+    <div>
+      <h1 className="flex flex-col justify-center items-center min-h-screen bg-black text-yellow-400 font-bold">
+        Favourites
+      </h1>
+    </div>
+  );
+};
+
+export default Favourites;
