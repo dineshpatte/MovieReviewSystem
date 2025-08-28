7ab8ee2a45516ebc369bb7ee5e79f56afa5b046b
dineshpatte <152471120+dineshpatte@users.noreply.github.com>
2025-08-28 19:49:03 +0530

    suggestions under serach bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import axios from "axios";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Search } from "lucide-react";
 
 interface searchBarProps {
@@ -11,6 +11,32 @@ interface searchBarProps {
 }
 
 const SearchBar = ({ movieName, setMovieName, onSearch }: searchBarProps) => {
+  const [suggestions, setSuggestions] = useState<any[]>([]);
+  useEffect(() => {
+    const fetchSuggestions = async () => {
+      if (!movieName.trim()) {
+        setSuggestions([]);
+        return;
+      }
+      try {
+        const res = await axios.get("https://www.omdbapi.com/", {
+          params: {
+            s: movieName,
+            apiKey: process.env.NEXT_PUBLIC_OMDB_KEY,
+          },
+        });
+        setSuggestions(res.data.Search || []);
+      } catch (error) {
+        console.log("error fetching suggestions", error);
+      }
+    };
+    fetchSuggestions();
+  }, [movieName]);
+
+  const handleSelect = (movie: any) => {
+    setMovieName(movie.Title);
+    setSuggestions([]);
+  };
   return (
     <div className="fixed top-0 left-0">
       <input
@@ -21,6 +47,22 @@ const SearchBar = ({ movieName, setMovieName, onSearch }: searchBarProps) => {
         className="border px-3 py-2 rounded-md mr-2 w-64"
       />
       <button onClick={onSearch}>Search</button>
+
+      {suggestions.length > 0 &&
+        suggestions.map((movie): any => (
+          <ul
+            className="absolute bg-white text-black border rounded-md w-full mt-1 max-h-60 overflow-y-auto shadow"
+            onClick={onSearch}
+          >
+            <li
+              key={movie.imdbID}
+              onClick={() => handleSelect(movie)}
+              className="px-3 py-2 hover:bg-gray-200 cursor-pointer"
+            >
+              {movie.Title} ({movie.Year})
+            </li>
+          </ul>
+        ))}
     </div>
   );
 };
